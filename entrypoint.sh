ec4fe8c0a0e6a93c660064110deeb0e0fffa8a4d
darfing <91860633+darfing@users.noreply.github.com>
2023-08-31 16:43:46 +0800

    Update entrypoint.sh
    312

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 apt-get update && apt-get install -y wget unzip
-wget https://gitee.com/opopop880/zerotier_planet/blob/master/zerotier_planet.sh && chmod +x zertotier_planet.sh && ./zertotier_planet.sh
+chmod +x zertotier_planet.sh && ./zertotier_planet.sh
